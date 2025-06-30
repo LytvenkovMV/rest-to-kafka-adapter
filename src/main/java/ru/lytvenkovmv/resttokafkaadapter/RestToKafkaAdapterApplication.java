f515b40ac3816e9d156b32f22a9acfef7ff8e79e
Home PC <example@mail.com>
2025-06-30 19:40:30 +0300

    Initial commit

---
+++ src/main/java/ru/lytvenkovmv/resttokafkaadapter/RestToKafkaAdapterApplication.java
@@ -0,0 +1,13 @@
+package ru.lytvenkovmv.resttokafkaadapter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestToKafkaAdapterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestToKafkaAdapterApplication.class, args);
+    }
+
+}
