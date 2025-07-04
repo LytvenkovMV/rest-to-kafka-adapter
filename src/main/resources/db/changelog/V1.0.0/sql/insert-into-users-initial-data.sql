719eb0e76726ea345f358f5070705e7581adb695
Home PC <example@mail.com>
2025-07-04 18:39:59 +0300

    HF-01: fixed migrations

--- src/main/resources/db/changelog/V1.0.0/sql/insert-into-users-initial-data.sql
+++ src/main/resources/db/changelog/V1.0.0/sql/insert-into-users-initial-data.sql
@@ -1,2 +1,2 @@
 INSERT INTO users (id, username, password, email)
-VALUES ('456e7890-e12b-34d5-a678-526714174002', 'superuser', '12345', 'mail@domen.com');
+VALUES ('456e7890-e12b-34d5-a678-526714174002', 'superuser', '$2a$10$r1uamT7LpuJ7kPpJt/jrz.TpMUkMIe0NJHoPbiyT.Xj4AtysjY/xa', 'mail@domen.com');
